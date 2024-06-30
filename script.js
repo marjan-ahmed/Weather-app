554be922b84f29cdcab20743053c9f0ee1186c52
marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-06-30 19:24:14 +0500

    first commit

--- script.js
+++ script.js
@@ -31,19 +31,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
             switch(weatherData.weather[0].main){
                 case 'Clouds':
-                    weather_img.src = "/images/cloud.png";
+                    weather_img.src = "/assets/cloud.png";
                     break;
                 case 'Clear':
-                    weather_img.src = "/images/clear.png";
+                    weather_img.src = "/assets/clear.png";
                     break;
                 case 'Rain':
-                    weather_img.src = "/images/rain.png";
+                    weather_img.src = "/assets/rain.png";
                     break;
                 case 'Mist':
-                    weather_img.src = "/images/mist.png";
+                    weather_img.src = "/assets/mist.png";
                     break;
                 case 'Snow':
-                    weather_img.src = "/images/snow.png";
+                    weather_img.src = "/assets/snow.png";
                     break;
             }
 
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         } catch (error) {
             console.error(error);
-            weather_img.src = "/images/404.png";
+            weather_img.src = "/assets/404.png";
             temperature.innerHTML = 'N/A';
             city.innerHTML = 'City not found';
             country.innerHTML = '';
